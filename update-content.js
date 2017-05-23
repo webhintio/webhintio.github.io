7d0a846734cebe057a103a65bf077ba3dd47d798
Cătălin Mariș <alrraa@gmail.com>
2017-05-23 23:54:20 +0300

    Improve `update-content.js`

--- update-content.js
+++ update-content.js
@@ -1,14 +1,21 @@
-const shell = require('shelljs');
+/* global config cp exec mkdir rm */
 
-const SOURCE_DIR = 'hexo/source';
-const TMP_DIR = 'tmp/sonar';
+const os = require('os');
+const path = require('path');
+
+const mktemp = require('mktemp');
+const shell = require('shelljs/global'); // eslint-disable-line no-unused-vars
+
 const CLONE_URL = process.env.TRAVIS ? 'git@github.com-sonar:MicrosoftEdge/Sonar.git' : 'https://github.com/MicrosoftEdge/Sonar.git'; // eslint-disable-line no-process-env
+const SOURCE_DIR = 'hexo/source';
+const TMP_DIR = mktemp.createDirSync(path.join(os.tmpdir(), '/XXXXX')); // eslint-disable-line no-sync
 
-if (shell.exec(`git clone ${CLONE_URL}  "${TMP_DIR}"`).code === 0) {
-    shell.rm('-rf', `${SOURCE_DIR}/docs`);
-    shell.cp('-R', `${TMP_DIR}/docs`, `${SOURCE_DIR}/docs`);
-    shell.mkdir('-p', `${SOURCE_DIR}/changelog`);
-    shell.cp(`${TMP_DIR}/CHANGELOG.md`, `${SOURCE_DIR}/changelog/index.md`);
-}
+config.fatal = true;
 
-shell.rm('-rf', TMP_DIR);
+exec(`git clone ${CLONE_URL}  "${TMP_DIR}"`);
+rm('-rf', `${SOURCE_DIR}/docs`);
+cp('-R', `${TMP_DIR}/docs`, `${SOURCE_DIR}/docs`);
+mkdir('-p', `${SOURCE_DIR}/changelog`);
+cp(`${TMP_DIR}/CHANGELOG.md`, `${SOURCE_DIR}/changelog/index.md`);
+
+rm('-rf', TMP_DIR);
