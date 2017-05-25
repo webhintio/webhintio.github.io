44c3d67d655356a77d5f3974f418ba2ba3d5c4c6
Cătălin Mariș <alrraa@gmail.com>
2017-05-25 09:58:54 +0300

    Make minor improvements to the automation setup

---
+++ .travis/is-travis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+[ "$TRAVIS" = "true" ] || exit 1
--- .travis/update-website-branch.sh
+++ .travis/update-website-branch.sh
@@ -3,31 +3,45 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/.." \
     || exit 1
 
+declare -r TMP_DIR="$(mktemp -d XXXXX)"
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-remove_unneeded_files() {
-    find . \
-        -maxdepth 1 \
-        ! -name "." \
-        ! -name "dist" \
-        ! -name "node_modules" \
-        ! -name "package.json" \
-        ! -name "src" \
-        ! -name "web.config" \
-        -exec rm -rf {} +;
+prepare_dist_dir() {
+
+    declare -r files=(
+        dist
+        package.json
+        src
+        web.config
+    )
+
+    for file in "${files[@]}"; do
+        cp -R "$file" "$TMP_DIR"
+    done
+}
+
+update_website_branch() {
+
+    # Automatically update the content from the `website` branch.
+    #
+    # Note: The SSH key is stored on Travis CI, but will need to be
+    #       added once the project is open sourced.
+    #
+    #       https://docs.travis-ci.com/user/private-dependencies/
+
+
+    "$(npm bin)/update-branch" --commands "echo" \
+                               --commit-message "Hey server, this content is for you! [skip ci]" \
+                               --directory "$TMP_DIR" \
+                               --distribution-branch "website" \
+                               --source-branch "master"
+
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Automatically update the content from the `website` branch.
-#
-# Note: The SSH key is stored on Travis CI, but will need to be
-#       added once the project is open sourced.
-#
-#       https://docs.travis-ci.com/user/private-dependencies/
+prepare_dist_dir \
+    && update_website_branch
 
-remove_unneeded_files \
-    && "$(npm bin)/update-branch" --commands "echo" \
-                                  --commit-message "Hey server, this content is for you! [skip ci]" \
-                                  --distribution-branch "website" \
-                                  --source-branch "master"
+rm -rf "$TMP_DIR"
