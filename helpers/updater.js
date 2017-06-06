e75eb8984758eae12917e3369aec1088e02c99ba
qingzhou <qzhou@plainconcepts.com>
2017-06-06 16:09:41 -0700

    Fix link in collectors page
    * Delete code in `updater.js` to update local links.
    * Use `hexo-hrefmd`.
    * This will fix the link problem once MicrosoftEdge/Sonar#247 is merged.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #119

--- helpers/updater.js
+++ helpers/updater.js
@@ -96,32 +96,6 @@ const addFrontMatter = async (filePath) => {
     await fs.writeFile(filePath, newData);
 };
 
-// Update local file links to site links
-// before: 'see [how to develop a collector](../developer-guide/collectors/how-to-develop-a-collector.md)'
-// after:  'see [how to develop a collector](/docs/developer-guide/collectors/how-to-develop-a-collector.html)'
-const updateLocalLinks = async (filePath) => {
-    const fileContent = await fs.readFile(filePath, 'utf8');
-    const localLinksRegex = /\((?!http)([^(|)]+.md)\)/g; // find parenthesis-enclosed content that ends with `.md` and doesn't start with `http`.
-    const localLinks = [];
-    let match;
-
-    while ((match = localLinksRegex.exec(fileContent)) !== null) {
-        localLinks.push(match.pop());
-    }
-
-    if (!localLinks.length) {
-        return;
-    }
-
-    const newFileContent = _.reduce(localLinks, (newContent, localLink) => {
-        const siteLink = `/${permaLinks.get(path.basename(localLink, '.md'))}`;
-
-        return newContent.replace(localLink, siteLink);
-    }, fileContent);
-
-    await fs.writeFile(filePath, newFileContent);
-};
-
 // Iterate all the markdown files and add frontmatter to each file
 klaw(directory)
     .on('data', (item) => {
@@ -137,9 +111,4 @@ klaw(directory)
 
         await Promise.all(addFrontMatterPromises);
         console.log('Front Matter added to each file.');
-
-        const updateLocalLinkPromises = filePaths.map(updateLocalLinks);
-
-        await Promise.all(updateLocalLinkPromises);
-        console.log('Links updated in each file.');
     });
