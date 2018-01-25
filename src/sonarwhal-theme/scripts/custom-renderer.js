279b2e4543dd8bda2a862b4fa1ffd4eafd2d2513
molant <amolleda@gmail.com>
2018-01-24 16:13:25 -0800

    Fix: Render properly `<details><summary`
    This fixes several issues we were having with the `<details>` element.
    Notably, it does:
    
    * Change from `hexo-renderer-marked` to a custom renderer that
      uses `remarkable`. This project currently parses markdown inside
      `<details>`
    * Adds a polyfill for browsers that do not support `<details>` and style
      to render it as a block
    * Removes custom code to add `noreferrer noopener` to external links in
      favor of a `remarkable` plugin
    * Updates how we add the `id` and anchor to headers
    * Changes how we `slugify` the `id` of the header anchors
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix #367
    Fix #370
    Fix #372

---
+++ src/sonarwhal-theme/scripts/custom-renderer.js
@@ -0,0 +1,46 @@
+/* global hexo */
+
+const Remarkable = require('remarkable');
+const extLink = require('remarkable-extlink');
+const toc = require('markdown-toc');
+const { stripHTML } = require('hexo-util');
+
+const renderer = function (data) {
+    const remarkable = new Remarkable({
+        breaks: false,
+        highlight(/*str, lang*/) {
+            return '';
+        },
+        html: true,
+        linkify: true,
+        typographer: true
+    });
+
+    remarkable.use(extLink, { host: 'sonarwhal.com' });
+
+    remarkable.use((rmkbl) => {
+        rmkbl.renderer.rules.heading_open = function (tokens, idx) { //eslint-disable-line camelcase
+            const content = tokens[idx + 1].content;
+            const id = toc.slugify(content);
+            const level = tokens[idx].hLevel;
+
+            return `<h${level} id=${id}>`;
+        };
+
+        rmkbl.renderer.rules.heading_close = function (tokens, idx) { //eslint-disable-line camelcase
+            const level = tokens[idx].hLevel;
+            const content = tokens[idx - 1].content;
+            const id = toc.slugify(content);
+
+            return `<a href="#${id}" class="headerlink" title="${stripHTML(content)}"></a></h${level}>`;
+        };
+    });
+
+    /* eslint-enable */
+
+    const remarkableText = remarkable.render(data.text);
+
+    return remarkableText;
+};
+
+hexo.extend.renderer.register('md', 'html', renderer, true);
