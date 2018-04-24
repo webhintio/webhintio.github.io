6283960b354c03c60e7849cd7c081e8f61e9317e
molant <amolleda@gmail.com>
2018-04-24 16:00:15 -0700

    New: Add GDPR banner
    Fix #427
    Close #428

---
+++ src/sonarwhal-theme/source/js/gdpr.js
@@ -0,0 +1,55 @@
+/* eslint-env browser */
+/* eslint-disable no-var, prefer-template, strict, prefer-arrow-callback, object-shorthand, no-continue */
+(function () {
+    /** Creates a cookie. Code based on https://www.quirksmode.org/js/cookies.html */
+    var createCookie = function (name, value, days) {
+        var expires = '';
+
+        if (days) {
+            var date = new Date();
+
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = '; expires=' + date.toUTCString();
+        }
+
+        document.cookie = name + '=' + value + expires + '; path=/';
+    };
+
+    /** Reads a cookie. Code based on https://www.quirksmode.org/js/cookies.html */
+    var readCookie = function (name) {
+        var nameEQ = name + '=';
+        var ca = document.cookie.split(';');
+
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1, c.length);
+            }
+
+            if (c.indexOf(nameEQ) === 0) {
+                return c.substring(nameEQ.length, c.length);
+            }
+        }
+
+        return null;
+    };
+
+    var hideDisclaimer = function () {
+        var ele = document.getElementById('disclaimer');
+
+        ele.setAttribute('aria-hidden', 'true');
+    };
+
+    var consent = readCookie('consent') === 'on';
+
+    document.querySelector('.close')
+        .addEventListener('click', hideDisclaimer, false);
+
+    if (consent) {
+        // user already gave consent so we start analytics
+        hideDisclaimer();
+    }
+
+    createCookie('consent', 'on', 30);
+}());
