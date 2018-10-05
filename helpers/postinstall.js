3e6506f70abdd0e5c2fda103234d42fa4ae284f8
Rory Duncan <robust.rory@gmail.com>
2018-10-05 13:41:15 +0000

    Fix: Update Code Style to pass linting

--- helpers/postinstall.js
+++ helpers/postinstall.js
@@ -7,17 +7,18 @@ const shell = require('shelljs');
 const baseSrcPath = 'node_modules/@hint/artwork/src/';
 const baseTargetPath = 'src/webhint-theme/source/images/';
 
-const fileSrcAndTargets = { 
-  'other/working/working.svg': 'nellie-construction.svg',
-  'other/octocat/octocat_1.svg': 'nellie-octocat.svg',
-  'other/confused/confused.svg': 'nellie-confused.svg',
-  'other/searching/searching.svg': 'nellie-searching.svg',
-  'other/developing/developing.svg': 'developer-nellie.svg',
-  'other/sunglasses/sunglasses.svg': 'nellie-customizable.svg',
+const fileSrcAndTargets = {
+    'other/confused/confused.svg': 'nellie-confused.svg',
+    'other/developing/developing.svg': 'developer-nellie.svg',
+    'other/octocat/octocat_1.svg': 'nellie-octocat.svg',
+    'other/searching/searching.svg': 'nellie-searching.svg',
+    'other/sunglasses/sunglasses.svg': 'nellie-customizable.svg',
+    'other/working/working.svg': 'nellie-construction.svg'
 };
 
-Object.entries(fileSrcAndTargets).forEach(entry => {
-  const srcPath = entry[0];
-  const targetPath = entry[1];
-  shell.cp(`${baseSrcPath}${srcPath}`, `${baseTargetPath}${targetPath}`);
+Object.entries(fileSrcAndTargets).forEach((entry) => {
+    const srcPath = entry[0];
+    const targetPath = entry[1];
+
+    shell.cp(`${baseSrcPath}${srcPath}`, `${baseTargetPath}${targetPath}`);
 });
