f4eaae4113ef6081a299e2e20e8a4332456ef2c9
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-01-31 10:38:53 -0800

    Fix: PR Feedback

--- helpers/update-site/constants.js
+++ helpers/update-site/constants.js
@@ -4,37 +4,37 @@ const USER_GUIDE = `${CONTENT}/docs/user-guide`;
 const REPOSITORY = require('../mktemp')();
 
 const dirs = {
-    // About documentation destination directory.
+    /** About documentation destination directory. */
     ABOUT: `${CONTENT}/about`,
-    // Destination directory for the content.
+    /** Destination directory for the content. */
     CONTENT,
-    // Contributor guide documentation destination directory.
+    /** Contributor guide documentation destination directory. */
     CONTRIBUTOR_GUIDE: `${CONTENT}/docs/contributor-guide`,
-    // Hexo _data folder
+    /** Hexo _data folder */
     DATA: `${CONTENT}/_data`,
-    // Hints documentation directory.
+    /** Hints documentation directory. */
     HINTS_DOC: `${USER_GUIDE}/hints`,
-    // Hint repository packages directory.
+    /** Hint repository packages directory. */
     HINT_PACKAGES: `${REPOSITORY}/packages`,
-    // Temporal directory to clone the repository.
+    /** Temporal directory to clone the repository. */
     REPOSITORY,
-    // Images for the HTML Formatter.
+    /** Images for the HTML Formatter. */
     SCAN_IMAGES: `${THEME}/source/images/scan`,
-    // EJS partial views compiled for the HTML Formatter.
+    /** EJS partial views compiled for the HTML Formatter. */
     SCAN_PARTIALS: `${THEME}/source/js/partials`,
-    // Scripts for the HTML Formatter.
+    /** Scripts for the HTML Formatter. */
     SCAN_SCRIPTS: `${THEME}/source/js/scan`,
-    // Styles necessary for the HTML Formatter.
+    /** Styles necessary for the HTML Formatter. */
     SCAN_STYLES: `${THEME}/source/core/css/scan`,
-    // Partial views for the HTML Formatter.
+    /** Partial views for the HTML Formatter. */
     SCAN_TEMPLATES: `${THEME}/layout/partials/scan`,
-    // User guide documentation destination directory.
+    /** User guide documentation destination directory. */
     USER_GUIDE
 };
 
 module.exports = {
-    // Repository to clone.
+    /** Repository to clone. */
     REPO_URL: 'https://github.com/webhintio/hint.git',
-    // Directories used to get the documentation from the hint repository.
+    /** Directories used to get the documentation from the hint repository. */
     dirs
 };
