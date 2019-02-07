0576baf68a399c0d2926efc438057bbb0b4c933d
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-02-07 15:47:09 -0800

    Chore: Improve documentation generator
    Fix #632
    Fix #631

--- helpers/update-site.js
+++ helpers/update-site.js
@@ -1,27 +1,8 @@
-/*
- * 1. Clean working directories.
- * 2. Clone repository.
- * 3. Copy hint files.
- * 4. Copy hints images.
- * 5. Copy Formatter html.
- * 6. Get documentation from each resource.
- * 7. Generate hints category information and documentation.
- * 8. Remove cloned repository.
- * 9. Add frontMatter info to documentation.
- * 10. Update documenattion links.
- * 11. Update Changelog.
- */
-
-const path = require('path');
-
-const globby = require('globby');
-
 const constants = require('./update-site/constants');
-const { addFrontMatter, updateChangelog, updateLinks } = require('./update-site/md');
 const { cloneRepo } = require('./update-site/clonerepo');
+const { copyFormatter } = require('./update-site/formatter');
+const { build, createHintCategories, createResourcesIndexes, getFiles, getFilesInfo, updateChangelog, updateLinks } = require('./update-site/documentation');
 const { cleanWorkingSpace, remove } = require('./update-site/remove');
-const { copyHTMLFormatter, copyHintDocumentation, copyResourceImages } = require('./update-site/copy');
-const { generateHintCategoryDocAndInfo, getDocumentation } = require('./update-site/resources');
 
 // 1. Clean working space.
 cleanWorkingSpace();
@@ -29,47 +10,58 @@ cleanWorkingSpace();
 // 2. Clone repository.
 cloneRepo(constants.REPO_URL, constants.dirs.REPOSITORY);
 
-// 3. Copy 'hint' documenation.
-copyHintDocumentation();
+// 3. Copy formatter.
+copyFormatter();
 
-/*
- * 4. Copy resources images.
- *
- * If another resource (formatter, connector, extension, etc.) contains
- * images, you need to create the directory and then copy.
- * e.g.:
- *   mkdirp.sync(`${DEST_DIR}/docs/user-guide/connectors/images`);
- *   cp('-R', `${PACKAGES_TMP_DIR}/connectors-* /images`, `${DEST_DIR}/docs/user-guide/connectors/images/`);
- *
- * NOTE: The space between * and / is intentional to not break the multiline comment.
- */
-copyResourceImages('hint');
-copyResourceImages('formatter');
+// 4. Get files from hint and resources (hints, formatters, etc.).
+getFiles()
+    .then((files) => {
+        // 5. Get docs information
+        return getFilesInfo(files);
+    })
+    .then((files) => {
+        /*
+         * 6. Create categories for hints.
+         *
+         * This will create as many file objects to files as categories
+         * we have for the hints.
+         * Also, this will create a file categores.json that will
+         * be use to build the index page for the HINTS section and
+         * an index page for each category.
+         *
+         * The layout that will use categories.json is hints-index.ejs
+         * and hints-category.ejs
+         */
+        createHintCategories(files);
 
-// 5. Copy formatter HTML.
-copyHTMLFormatter();
+        return files;
+    })
+    .then((files) => {
+        /*
+         * 7. Create index page for all the resources except hints.
+         *
+         * This will add as many file objects to files as type of
+         * resources we have: connectors, formatters, etc.
+         */
+        createResourcesIndexes(files);
 
-// 6. Get the documentation for the resources.
-getDocumentation();
+        return files;
+    })
+    .then((files) => {
+        // 8. Update documentation links.
+        updateLinks(files);
 
-// 7. Generate hints category information and documentation.
-generateHintCategoryDocAndInfo();
-
-// 8. Remove cloned repository
-remove(constants.dirs.REPOSITORY, '-rf');
-
-const docFiles = globby.sync(['**/*.md', '!404.md', '!about.md', '!contributors.md', '!docs/index.md'], {
-    absolute: true,
-    cwd: path.join(process.cwd(), constants.dirs.CONTENT)
-});
-
-// 9. Add frontMatter info to documentation.
-addFrontMatter(docFiles)
-    .then(() => {
-        // 10. Update documentation links.
-        return updateLinks(docFiles);
+        return files;
+    })
+    .then((files) => {
+        // 9. Build documentation files in dest.
+        return build(files);
     })
     .then(() => {
-        // 11. Update Changelog.
-        updateChangelog();
+        // 10. Update Changelog.
+        return updateChangelog();
+    })
+    .then(() => {
+        // 11. Remove repository.
+        remove(constants.dirs.REPOSITORY, '-rf');
     });
