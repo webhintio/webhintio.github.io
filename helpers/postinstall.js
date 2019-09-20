7bf35d9a597c34e2848f3db309014da845bc2d89
Antón Molleda <amolleda@gmail.com>
2019-09-20 16:09:49 -0700

    Build: Stop release if issues are find during deployment
    Fix #717

--- helpers/postinstall.js
+++ helpers/postinstall.js
@@ -2,26 +2,30 @@
  * Post Install Script
  * Copies a set of files from the `@hint/artwork` asset repository into this one
  */
-const shell = require('shelljs');
-const mkdirp = require('mkdirp');
+try {
+    const shell = require('shelljs');
+    const mkdirp = require('mkdirp');
 
-const baseSrcPath = 'node_modules/@hint/artwork/src/';
-const baseTargetPath = 'src/webhint-theme/source/images/';
+    const baseSrcPath = 'node_modules/@hint/artwork/src/';
+    const baseTargetPath = 'src/webhint-theme/source/images/';
 
-const fileSrcAndTargets = {
-    'other/confused/confused.svg': 'nellie-confused.svg',
-    'other/developing/developing.svg': 'developer-nellie.svg',
-    'other/octocat/octocat_1.svg': 'nellie-octocat.svg',
-    'other/searching/searching.svg': 'nellie-searching.svg',
-    'other/sunglasses/sunglasses.svg': 'nellie-customizable.svg',
-    'other/working/working.svg': 'nellie-construction.svg'
-};
+    const fileSrcAndTargets = {
+        'other/confused/confused.svg': 'nellie-confused.svg',
+        'other/developing/developing.svg': 'developer-nellie.svg',
+        'other/octocat/octocat_1.svg': 'nellie-octocat.svg',
+        'other/searching/searching.svg': 'nellie-searching.svg',
+        'other/sunglasses/sunglasses.svg': 'nellie-customizable.svg',
+        'other/working/working.svg': 'nellie-construction.svg'
+    };
 
-mkdirp.sync(baseTargetPath);
+    mkdirp.sync(baseTargetPath);
 
-Object.entries(fileSrcAndTargets).forEach((entry) => {
-    const srcPath = entry[0];
-    const targetPath = entry[1];
+    Object.entries(fileSrcAndTargets).forEach((entry) => {
+        const srcPath = entry[0];
+        const targetPath = entry[1];
 
-    shell.cp(`${baseSrcPath}${srcPath}`, `${baseTargetPath}${targetPath}`);
-});
+        shell.cp(`${baseSrcPath}${srcPath}`, `${baseTargetPath}${targetPath}`);
+    });
+} catch (e) {
+    console.log(`Couldn't copy images, probably install in "--production" mode`);
+}
