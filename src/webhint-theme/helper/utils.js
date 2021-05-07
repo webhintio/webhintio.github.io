9b9b8f102fa8c8648f7447acba6b716ece9b1bd4
Rachel Simone Weil <partytimehexcellent@gmail.com>
2021-05-07 08:20:37 -0500

    Removes getSignalIssueQuery

--- src/webhint-theme/helper/utils.js
+++ src/webhint-theme/helper/utils.js
@@ -77,12 +77,6 @@ module.exports = {
             return page.section === section;
         });
     },
-    getSignalIssueQuery: (root, title, directory) => {
-        const issueTitle = `[docs] Issue with '${title}'`;
-        const issueContent = url.resolve(root, directory);
-
-        return `title=${encodeURIComponent(issueTitle)}&body=${encodeURIComponent(issueContent)}`;
-    },
     /**
      * Return the `href` value to link to the target page from the current one.
      * for example when the current page is `hint-amp-validator`:
